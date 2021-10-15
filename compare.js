5af31a445f3ee038db7874f70c24583f3e2f4e1b
Leigh V <lavillarroel@live.com>
2021-10-14 22:48:46 -0400

    Set up commit

--- compare.js
+++ compare.js
@@ -8,7 +8,7 @@
  * 
  *  Program flow
  *   1. Retrieve Input from user
- *   2. User axios (Also can use oxtokit) to request
+ *   2. User axios (Also can use oxtokit) to request from https://api.github.com/${owner}/${reponame}/${branch}/${filepath}
  *   3.
  * 
  *   WIll need to use GitHub  Actions to test changes
@@ -17,16 +17,34 @@
 //Inputs read via dotenv
 require('dotenv').config();
 
+//Need to pass in process.env.TOKEN as a Header Authroization
+
 //Axios for call
  var axios = require('axios');
 
 
- //Sample Get to GitHub
- axios.get('https://api.github.com/zen')
+// A Sample Zen call to GitHub
+//  axios.get('https://api.github.com/zen', { headers: { 'Authorization': `${process.env.TOKEN}` } })
+//    .then(function (response) {
+//      console.log(response);
+//    })
+//    .catch(function (error) {
+//      //Handle error
+//      console.log(error);
+//    })
+
+
+//Get Branch Repo calls
+//https://api.github.com/repos/${owner}/${reponame}/commits
+
+let owner = 'geerlingguy';
+let repo = 'ansible-for-devops';
+
+axios.get(`https://api.github.com/repos/${owner}/${repo}/commits`, { headers: { 'Authorization': `${process.env.TOKEN}` } })
    .then(function (response) {
-     console.log(response.data);
+     console.log(response);
    })
    .catch(function (error) {
      //Handle error
-     console.log(error);
+     console.log('We could not find any information with the parameters provided' + error);
    })
