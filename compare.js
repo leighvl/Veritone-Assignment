fd69497c1afaf702f53f7d8e3d1f76ff44f5ceaf
Leigh V <lavillarroel@live.com>
2021-10-17 13:21:51 -0400

    Add chained gets

--- compare.js
+++ compare.js
@@ -7,7 +7,11 @@
  *  Use Node and GitHub API must complete by Sunday
  * 
  *  Program flow
- *   1. Retrieve Input from user
+ *   1. Retrieve Input from user repositor
+ *   2. GET list of tags for repo
+ *   3. store latest and oldest commits
+ *   4. Run another GET request with commit info
+ *   5. Parse JSON
  *   2. User axios (Also can use oxtokit) to request from https://api.github.com/${owner}/${reponame}/${branch}/${filepath}
  *   3.
  * 
@@ -17,28 +21,43 @@
 //Inputs read via dotenv
 require('dotenv').config();
 
-//Need to pass in process.env.TOKEN as a Header Authroization
+//Need to pass in process.env.TOKEN as a Header Authroization, other variables as well
 
  var axios = require('axios');
  var ghAPI = 'https://api.github.com';
+ let owner = 'geerlingguy';
+ let repo = 'ansible-for-devops';
 
 
 
 
-<<<<<<< HEAD
-//Get Repo commit calls using GitHub API
+//Get Repo commit tags
 
-// //https://api.github.com/repos/${owner}/${repo}/commits
-// let owner = 'geerlingguy';
-// let repo = 'ansible-for-devops';
-=======
-//Get Repo commit calls and compare two Commits
+axios.get(`${ghAPI}/repos/${owner}/${repo}/tags`, { headers: { 'Authorization': `token ${process.env.TOKEN}` } })
+  .then(function (response){
+    var tagsObject = response.data;
+    //console.log(tagsObject[Object.keys(tagsObject)[Object.keys(tagsObject).length - 1 ]])
+
+    //Get first(base) and last(head) entries in object
+    let head_tag = response.data[0].name
+    let base_tag = tagsObject[Object.keys(tagsObject)[Object.keys(tagsObject).length - 1 ]].name
+    
+
+    console.log(`${head_tag} & ${base_tag}`)
+
+    // Do Second get here (Can optimize with Promise.all)
+  })
+  .then()
+  .catch(function (error) {
+         //Handle error
+         console.log('We could not find any information with the parameters provided' + error);
+       });
+
+//and compare two Commits
 
 // //https://api.github.com/repos/${owner}/${reponame}/compare/{basehead}
-// let owner = 'geerlingguy';
-// let repo = 'ansible-for-devops';
+
 // let basehead = 'basehead';
->>>>>>> 3afd282e68b1073f4f492329a8b0965bcb446380
 
 // axios.get(`https://api.github.com/repos/${owner}/${repo}/commits`, { headers: { 'Authorization': `token ${process.env.TOKEN}` } })
 //    .then(function (response) {
@@ -67,59 +86,42 @@ require('dotenv').config();
 //    })
 
 
-<<<<<<< HEAD
 //Get Repo ccompare two Commits
 //Need to get base commit and head commit values.
 
 
-//https://api.github.com/repos/${owner}/${reponame}/compare/{basehead}
-let owner = 'geerlingguy';
-let repo = 'ansible-for-devops';
-let basehead = 'basehead'; // the names of the branch names
+//https://api.github.com/repos/${owner}/${reponame}/compare/{base}...{head}  where {base} and {head} are branch names/commits, oldest first
+// let owner = 'geerlingguy';
+// let repo = 'ansible-for-devops';
+// let base = 'master'; //oldest branch
+// let head = 'basehead'; //latest branch
 
-axios.get(`https://api.github.com/repos/${owner}/${repo}/compare/${basehead}`, { headers: { 'Authorization': `token ${process.env.TOKEN}` } })
-   .then(function (response) {
-    if (response.status == 200 ) {
-      //Put response.data [Object]
-      var commits = response.base_commit;
-=======
-//Get Repo commit calls and compare two Commits
-
-//https://api.github.com/repos/${owner}/${reponame}/compare/{basehead}
-let owner = 'geerlingguy';
-let repo = 'ansible-for-devops';
-let basehead = 'basehead';
-
-axios.get(`https://api.github.com/repos/${owner}/${repo}/commits`, { headers: { 'Authorization': `token ${process.env.TOKEN}` } })
-   .then(function (response) {
-    if (response.status == 200 ) {
-      //Put response.data [Object]
-      var commits = response.data;
->>>>>>> 3afd282e68b1073f4f492329a8b0965bcb446380
-      console.log(commits)
-      //Traverse object
-      // for (let [key,obj] of Object.entries(commits)) {
+// axios.get(`https://api.github.com/repos/${owner}/${repo}/compare/${basehead}`, { headers: { 'Authorization': `token ${process.env.TOKEN}` } })
+//    .then(function (response) {
+//     if (response.status == 200 ) {
+//       //Put response.data [Object]
+//       var commits = response.base_commit;
+//       console.log(commits)
+//       //Traverse object
+//       // for (let [key,obj] of Object.entries(commits)) {
         
-      //   for (let [key,value] of Object.entries(obj)) {
-      //      console.log(`${key}: ${value}`)
-      //     }
-      // }
+//       //   for (let [key,value] of Object.entries(obj)) {
+//       //      console.log(`${key}: ${value}`)
+//       //     }
+//       // }
       
-      //Determine the head and base commit respectively
+//       //Determine the head and base commit respectively
 
-    } 
-    //Not found
+//     } 
+//     //Not found
     
 
-   })
-   .catch(function (error) {
-     //Handle error
-     console.log('We could not find any information with the parameters provided' + error);
-   })
+//    })
+//    .catch(function (error) {
+//      //Handle error
+//      console.log('We could not find any information with the parameters provided' + error);
+//    })
 
 
 
 
-// var head_commit = axios.get(
-
-// );
