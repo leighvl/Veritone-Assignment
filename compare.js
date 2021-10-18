d25094195044721c94691ec45ca8a917d0214e54
Leigh V <lavillarroel@live.com>
2021-10-18 00:46:25 -0400

    Pre github actions integration

--- compare.js
+++ compare.js
@@ -1,21 +1,12 @@
+#!/usr/bin/env node
 /** DevOps Coding Challenge for Veritone
  * 
  *  Inputs: GitHub Organization, Repository Name, Head commit, Base commit
  * 
  *  Output: comparison printout between the two commits
  * 
- *  Use Node and GitHub API must complete by Sunday
+ *  Send link to Karma
  * 
- *  Program flow
- *   1. Retrieve Input from user repositor
- *   2. GET list of tags for repo
- *   3. store latest and oldest commits
- *   4. Run another GET request with commit info
- *   5. Parse JSON
- *   2. User axios (Also can use oxtokit) to request from https://api.github.com/${owner}/${reponame}/${branch}/${filepath}
- *   3.
- * 
- *   WIll need to use GitHub  Actions to test changes
  */
  
 //Inputs read via dotenv
@@ -34,58 +25,60 @@ require('dotenv').config();
 //Get Repo commit tags
 //Need alternative header if TOKEN is not set
 
-axios.get(`${ghAPI}/repos/${owner}/${repo}/tags`, { headers: { 'Authorization': `token ${process.env.TOKEN}` } })
-  .then(function (response){
-    var tagsObject = response.data;
-    //console.log(tagsObject[Object.keys(tagsObject)[Object.keys(tagsObject).length - 1 ]])
+const compareCommits = ( ghAPI = 'https://api.github.com' , owner, repo ) => {
+          axios.get(`${ghAPI}/repos/${owner}/${repo}/tags`, { headers: { 'Authorization': `token ${process.env.TOKEN}` } })
+            .then(function (response){
+              var tagsObject = response.data;
+              //console.log(tagsObject[Object.keys(tagsObject)[Object.keys(tagsObject).length - 1 ]])
 
-    //Get first(base) and last(head) entries in object
-    let head_tag = response.data[0].name
-    let base_tag = tagsObject[Object.keys(tagsObject)[Object.keys(tagsObject).length - 1 ]].name
-    
+              //Get first(base) and last(head) entries in object
+              let head_tag = response.data[0].name
+              let base_tag = tagsObject[Object.keys(tagsObject)[Object.keys(tagsObject).length - 1 ]].name
+              
 
-    console.log(`Found Head: ${head_tag} & Base: ${base_tag}`)
+              console.log(`Found Head: ${head_tag} & Base: ${base_tag}`)
 
-    // Do Second get here to compare the commits (Can optimize with Promise.all)
-    //Will also need a check to see if range is too large to even compare
+              // Do Second get here to compare the commits (Can optimize with Promise.all)
+              //Will also need a check to see if range is too large to even compare
 
-    //https://api.github.com/repos/${owner}/${reponame}/compare/{base}...{head}  where {base} and {head} are branch names/commits, oldest first
-      axios.get(`${ghAPI}/repos/${owner}/${repo}/compare/${base_tag}...${head_tag}`, { headers: { 'Authorization': `token ${process.env.TOKEN}` } })
-      .then( function (response) {
-        //Would this be the second get | put the compare code here
-        if (response.status == 200 ) {
-          //Get list of commits messages via response.data.commits[0].commit.message
-
-
-          console.log(response.data)
-          var commitObject = response.data.commits;
-          
-         
-          // for ( var i in commitObject) {
-          //   // if (commitObject.hasOwnProperty(i)) {
-          //   //     console.log(`Commit.${i} = ${commitObject[i]}\n`)
-          //   // }
-          //  // Log messages
-          //   console.log(response.data.commits[i].commit.message)
-          // }
-        
-          console.log(`More detailed of file changes can be access via ${response.data.diff_url}`)
-        }
-      }
-      )
+              //https://api.github.com/repos/${owner}/${reponame}/compare/{base}...{head}  where {base} and {head} are branch names/commits, oldest first
+                axios.get(`${ghAPI}/repos/${owner}/${repo}/compare/${base_tag}...${head_tag}`, { headers: { 'Authorization': `token ${process.env.TOKEN}` } })
+                .then( function (response) {
+                  //Would this be the second get | put the compare code here
+                  if (response.status == 200 ) {
+                    //Get list of commits messages via response.data.commits[0].commit.message
 
 
-    //Consider using .Diff option  on response.data.diff_url
+                   
+                    var commitObject = response.data.commits;
+                    var commitAuthorObject = response.data.commits;
+                    //console.log(commitObject)
+                  
+                    for ( var i in commitObject) {
+                      // if (commitObject.hasOwnProperty(i)) {
+                      //     console.log(`Commit.${i} = ${commitObject[i]}\n`)
+                      // }
+                     // Log messages
+                      console.log(response.data.commits[i].commit.message)
+                    }
+                  
+                    console.log(`More detailed of file changes can be access via ${response.data.diff_url}`)
+                  }
+                }
+                )
 
-  })
-  .catch(function (error) {
-         //Handle error
-         console.log('We could not find any information with the parameters provided: \n' + error);
-       });
-  
+
+              //Consider using .Diff option  on response.data.diff_url
+
+            })
+            .catch(function (error) {
+                  //Handle error
+                  console.log('We could not find any information with the parameters provided: \n' + error);
+                });
+            
 
 
+      };
 
 
-
-
+compareCommits(ghAPI, owner, repo)
